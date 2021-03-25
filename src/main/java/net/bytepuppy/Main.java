4d3fb2136593bd80adfbcc94ecda13e41b4c8f26
hong.liu <hong.liu@dmall.com>
2021-03-25 09:45:13 +0800

    init

---
+++ src/main/java/net/bytepuppy/Main.java
@@ -0,0 +1,13 @@
+package net.bytepuppy;
+
+/**
+ * @author liuh
+ * @email hong.liu@dmall.com
+ * @date 2021/3/25
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("234");
+    }
+}
---
+++ src/main/java/net/bytepuppy/subclass/CreateClassMain.java
@@ -0,0 +1,9 @@
+package net.bytepuppy.subclass;
+
+/**
+ * @author liuh
+ * @email hong.liu@dmall.com
+ * @date 2021/3/25
+ */
+public class CreateClassMain {
+}
